cd29ce2557f440230dc28444ce725410b401415b
Joshua Rinard and Marty Kovach <Guest@epicodus-87.local>
2016-12-12 16:55:09 -0800

    add commits

--- lib/task.rb
+++ lib/task.rb
@@ -1,15 +1,18 @@
 class Task < ActiveRecord::Base
   belongs_to(:list)
+  # validates empty field
   validates(:description, :presence => true)
+  # validates length of text
   validates(:description, {:presence => true, :length => { :maximum => 50 }})
   before_save(:downcase_description)
-  
+
   scope(:not_done, -> do
     where({:done => false})
   end)
 
 private
 
+  # Makes input downcase as it is saved
   define_method(:downcase_description) do
     self.description=(description().downcase())
   end
